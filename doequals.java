15c0832ec8dbd1ac729d1f135847ee0ba5ed75c8
Jayamurugan0001 <mukeshkanna110@gmail.com>
2024-12-03 13:12:06 +0530

    Add files via upload

---
+++ doequals.java
@@ -0,0 +1,17 @@
+package day8;
+
+public class doequals {
+	public static void main(String[] args) {
+		String name ="surya";
+		String str = new String("dead");
+		
+		if(name.equals(str)) {
+			System.out.print("surya weds megna");
+			
+		}else{
+			System.out.print("surya weds ramya");
+		}
+	}
+ 
+	
+}
